8bbd7b13cbee8504c9cf59d3443dda978b20447d
Trot-dms <trot.dms@gmail.com>
2016-11-06 15:55:49 +0100

    Class for getting Addins and Modules

---
+++ PDMS-Addins/AddinListManipulation.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PDMS_Addins
+{
+    class AddinListManipulation
+    {
+        
+        public List<Addin> AddinList
+        {
+            get
+            {
+                return GetAddinList(pdmsFolder);
+            }
+        }
+
+        public List<Module> ModuleList
+        {
+            get
+            {
+                return GetModuleList(pdmsFolder);
+            }
+        }
+
+        private string pdmsFolder;
+
+
+
+        public AddinListManipulation()
+        {
+            //bez podania ścieżki do folderu PDMSa użyj ścieżki programu
+            pdmsFolder = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+        }
+        public AddinListManipulation(string pdmsFolder)
+        {
+            this.pdmsFolder = pdmsFolder;
+        }
+
+        private List<Module> GetModuleList(string pdmsFolder)
+        {
+            
+            List<Module> tempModule = new List<Module>();
+            string[] tfiles = Directory.GetFiles(pdmsFolder, "*Addins.xml", SearchOption.TopDirectoryOnly);
+            foreach (string file in tfiles)
+            {
+                Module module = new Module();
+                module.ModuleFolder = pdmsFolder;
+                module.ModuleName = Path.GetFileName(file);
+                tempModule.Add(module);
+            }
+
+                return tempModule;
+        }
+        private List<Addin> GetAddinList(string pdmsFolder)
+        {
+           
+            List<Addin> addinList = new List<Addin>();
+
+            //tymczasowa lista modułów
+            List<Module> tempModule = new List<Module>();
+            List<string> tempAddins = new List<string>();
+
+            tempModule.AddRange(GetModuleList(pdmsFolder));
+
+            foreach(Module m in tempModule)
+            {
+                tempAddins.Clear();
+                tempAddins.AddRange(getAddinsForModule(m.ModuleName));
+                foreach (string item in tempAddins)
+                {
+                    Addin addin = new Addin();
+                    addin.AddinFolder = pdmsFolder;
+                    addin.AddinModule = m;
+                    addin.AddinName = item;
+                    addin.AddinState = true; //skoro wczytujemy go z plików to znaczy że jest włączony (true)
+                    addinList.Add(addin);
+                }
+            }
+
+            
+
+            return addinList;
+        }
+
+        private List<string> getAddinsForModule(string module)
+        {
+            List<string> addinList = new List<string>();
+
+            using (XmlReader reader = XmlReader.Create(pdmsFolder + @"\" + module))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "string":
+                                if (reader.Read())
+                                {
+                                    addinList.Add(reader.Value.Trim());
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            return addinList;
+        }
+
+
+    }
+}
