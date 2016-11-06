cd24ff92432001af5dcc694762aba08fefd58c67
Trot-dms <trot.dms@gmail.com>
2016-11-06 15:56:47 +0100

    Calss for storing Modules data.

---
+++ PDMS-Addins/Module.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDMS_Addins
+{
+    class Module
+    {
+        public string ModuleFolder { get; set; }
+        public string ModuleName { get; set; }
+
+    }
+}
